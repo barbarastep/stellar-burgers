4d0d4d975e2f1536c71c77f4b8b123cbd2fd72f2
Barbara Step <b.barbarastep@gmail.com>
2025-11-06 13:33:08 +0300

    fix: load ingredients on direct feed page visit

--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -3,11 +3,19 @@ import { FeedUI } from '@ui-pages';
 import { FC, useEffect } from 'react';
 import { useDispatch, useSelector } from '../../services/store';
 import { fetchFeeds } from '../../services/slices/feed-slice';
+import { fetchIngredients } from '../../services/slices/ingredients-slice';
 
 export const Feed: FC = () => {
   const dispatch = useDispatch();
   const { orders, isLoading } = useSelector((s) => s.feed);
+  const ingredients = useSelector((s) => s.ingredients.items);
   const placedOrder = useSelector((s) => s.order.currentOrder);
+
+  useEffect(() => {
+    if (!ingredients || ingredients.length === 0) {
+      dispatch(fetchIngredients());
+    }
+  }, [dispatch, ingredients]);
 
   useEffect(() => {
     dispatch(fetchFeeds());
