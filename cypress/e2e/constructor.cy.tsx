b3f3e0832a33fc4dbe58c6f790d96af7677f0855
Barbara Step <b.barbarastep@gmail.com>
2025-11-11 22:51:09 +0300

    test: add Cypress fixture and test for ingredients API intercept

---
+++ cypress/e2e/constructor.cy.tsx
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe('Ингредиенты загружаются из фикстуры', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/api/ingredients', { fixture: 'ingredients.json' }).as('getIngredients')
+    cy.intercept('GET', '**/api/auth/user', { statusCode: 200, body: { success: true, user: null } }).as('getUser')
+    cy.visit('/')
+  })
+
+  it('подменяет ответ и отображает данные фикстуры', () => {
+    cy.wait('@getIngredients')
+    cy.wait('@getIngredients')
+    cy.contains('Краторная булка N-200i').should('be.visible')
+    cy.contains('Биокотлета из марсианской Магнолии').should('be.visible')
+  })
+})
